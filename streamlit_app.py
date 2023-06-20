500a0dc6c8bd6df00853691be3dab32f03595c97
PeddapallySandhya <131743472+PeddapallySandhya@users.noreply.github.com>
2023-06-20 15:43:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
 
-st.set_page_config(page_title ="🦜🔗 Blog Outline Generator App")
-st.title('🦜🔗 Blog Outline Generator App')
+st.set_page_config(page_title ="🦜🔗 Outline Generator App")
+st.title('🦜🔗 Outline Generator App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 def generate_response(topic):
